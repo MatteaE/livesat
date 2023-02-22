0f85c4cd3e34b77377d9fbef4914cd38b8a69b61
ocirne94 <enrico.mattea@unifr.ch>
2023-02-22 12:20:00 +0100

    First website test

---
+++ script.js
@@ -0,0 +1,54 @@
+var img_basedir = "imagery/abramov"
+var img_names = ["2023-02-20_ls.jpg", "2023-02-20_s2.jpg"]
+
+
+var imgTag = document.querySelector("img")
+var geotiff16 = document.getElementById("geotiff16_dl")
+var geotiff8 = document.getElementById("geotiff8_dl")
+
+var count = img_names.length - 1
+
+var img_v = "v3"
+
+var geotiff16_names = img_names.map(x => x.replace("jpg", "tif"))
+var geotiff8_names = img_names.map(x => x.replace(".jpg", "_pan.tif"))
+
+var geotiff16_set = geotiff16_names.map(x => img_basedir + "/v1/" + x)
+var geotiff8_set = geotiff8_names.map(x => img_basedir + "/v2/" + x)
+
+function next() {
+    img_set = img_names.map(x => img_basedir + "/" + img_v + "/" + x)
+
+    count++
+    if (count >= img_set.length) {
+        count = 0
+    }
+    imgTag.src = img_set[count]
+    geotiff16.action = geotiff16_set[count]
+    geotiff8.action = geotiff8_set[count]
+}
+
+function prev() {
+    img_set = img_names.map(x => img_basedir + "/" + img_v + "/" + x)
+
+    count--
+    if (count < 0) {
+        count = img_set.length - 1
+    }
+    imgTag.src = img_set[count]
+    geotiff16.action = geotiff16_set[count]
+    geotiff8.action = geotiff8_set[count]
+}
+
+
+function toggle_contours() {
+    if (img_v == "v3") {
+        img_v = "v4"
+    } else {
+        img_v = "v3"
+    }
+
+    img_set = img_names.map(x => img_basedir + "/" + img_v + "/" + x)
+    imgTag.src = img_set[count]
+
+}
